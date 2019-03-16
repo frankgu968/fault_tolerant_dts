2e5a8837538e9d2415240424acf0a38c9ddca064
Frank Gu <frank.gu968@outlook.com>
2019-03-16 11:21:04 -0400

    Completed containerized seeder

---
+++ utils/seed_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONTAINER_NAME=database_seeder
+
+docker build . -t util-seeder
+docker rm -f $CONTAINER_NAME
+docker run  --network=fault_tolerant_dts_default \
+    -e DB_NAME='scheduler_db' \
+    -e TASK_COL_NAME='tasks' \
+    -e MONGO_HOST='fault_tolerant_dts_mongo_1' \
+    -e MONGO_PORT=27017 \
+    -e MONGO_USER='root' \
+    -e MONGO_PASSWORD='example' \
+    --name $CONTAINER_NAME \
+    util-seeder
---
+++ utils/seeder.py
@@ -0,0 +1,56 @@
+from pymongo import MongoClient
+from random import randint
+from os import environ
+import logging
+
+DB_NAME =        environ["DB_NAME"]
+TASK_COL_NAME =  environ["TASK_COL_NAME"]
+MONGO_HOST =     environ["MONGO_HOST"]
+MONGO_PORT =     environ["MONGO_PORT"]
+MONGO_USER =     environ["MONGO_USER"]
+MONGO_PASSWORD = environ["MONGO_PASSWORD"]
+
+
+def seed_db():
+    if not MONGO_PORT.isdigit():
+        logging.error("MONGO_PORT environment variable must be a positive number!")
+        exit(1)
+
+    client = MongoClient(MONGO_HOST,
+                         int(MONGO_PORT),
+                         username=MONGO_USER,
+                         password=MONGO_PASSWORD)
+
+    # Clean database and recreate
+    logging.info("Creating database " + DB_NAME)
+    dblist = client.list_database_names()
+    if DB_NAME in dblist:
+        client.drop_database(DB_NAME)
+    new_db = client[DB_NAME]
+
+    # Add task collection
+    logging.info("Adding collection " + TASK_COL_NAME)
+    task_col = new_db[TASK_COL_NAME]
+
+    # Seed 100 tasks in task collection
+    logging.info("Seeding tasks...")
+    for idx in range(100):
+        seed_task(idx, task_col)
+
+    logging.info("Database seeded!")
+
+def seed_task(index, collection):
+    new_task = {
+        "taskname": "task" + str(index),
+        "sleeptime": randint(1, 30),
+        "state": "created"
+    }
+    collection.insert_one(new_task)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
+    seed_db()
